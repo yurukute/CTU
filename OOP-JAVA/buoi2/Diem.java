ce0dfea8e5d09f0161a1496bbaaf341a9f0d8101
yurukute <nguyenkhanhdung.9999@gmail.com>
2021-10-17 16:28:10 +0700

    indenting

--- OOP-JAVA/buoi2/Diem.java
+++ OOP-JAVA/buoi2/Diem.java
@@ -6,47 +6,47 @@ public class Diem {
     private int x, y;
     
     public Diem(){
-	x = 0;
-	y = 0;
+		x = 0;
+		y = 0;
     }
     public Diem(int x1, int y1){
-	x = x1;
-	y = y1;
+		x = x1;
+		y = y1;
     }
     public Diem(Diem s){
-	x = s.x;
-	y = s.y;
+		x = s.x;
+		y = s.y;
     }
     public void nhap(){
-	Scanner sc = new Scanner(System.in);
-	x = sc.nextInt();
-	y = sc.nextInt();	
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		y = sc.nextInt();	
     }
     public void in(){
-	System.out.println("(" + x + ", " + y + ")");
+		System.out.println("(" + x + ", " + y + ")");
     }
     public void in(String s){
-	System.out.println(s);
-	System.out.println("(" + x + ", " + y + ")");
+		System.out.println(s);
+		System.out.println("(" + x + ", " + y + ")");
     }
     
     public String toString(){
-	return "(" + x + ", " + y + ")";
+		return "(" + x + ", " + y + ")";
     }
     public void doiDiem(int dx, int dy){
-	x += dx;
-	y += dy;
+		x += dx;
+		y += dy;
     }
     public int giatriX(){
-	return x;	   
+		return x;	   
     }
     public int giatriY(){
-	return y;
+		return y;
     }
     public double khoangCach(){
-	return Math.sqrt((x * x) + (y * y));
+		return Math.sqrt((x * x) + (y * y));
     }
     public double khoangCach(Diem d){
-	return Math.sqrt(Math.pow(x - d.giatriX(), 2) + Math.pow(y - d.giatriY(), 2));
+		return Math.sqrt(Math.pow(x - d.giatriX(), 2) + Math.pow(y - d.giatriY(), 2));
     }    
 }
--- OOP-JAVA/buoi2/PhanSo.java
+++ OOP-JAVA/buoi2/PhanSo.java
@@ -6,13 +6,13 @@ public class PhanSo {
     private int tu, mau;
 
     public PhanSo(){
-	tu  = 0;
-	mau = 1;
+		tu  = 0;
+		mau = 1;
     }
 
     public PhanSo(int a, int b){
-	tu  = a;
-	mau = b;
+		tu  = a;
+		mau = b;
     }
 
     public void nhap(){
@@ -20,79 +20,79 @@ public class PhanSo {
     	System.out.print("Tu so = ");
     	tu = sc.nextInt();
     	do{
-	    System.out.print("Mau so = ");
-	    mau = sc.nextInt();
-	    if(mau == 0)
-		System.out.println("Mau khong the bang 0!");
+			System.out.print("Mau so = ");
+			mau = sc.nextInt();
+			if(mau == 0)
+				System.out.println("Mau khong the bang 0!");
     	} while (mau == 0);
     }
 
     public void in(){	
-	if(mau < 0) tu = -tu;
-	/* Chuan hoa phan so
-	boolean dau = false;
-	if(tu < 0) {
-	    dau = true;
-	    tu = -tu;
-	}
-	for(int i = tu; i > 1; i--) {
-	    if(mau % i == 0 && tu % i == 0) {
-		mau /= i;
-		tu /= i;
-		break;
-	    }
-	}
-	if(dau) tu = -tu;*/
-	System.out.println(tu == 0 ? 0 : mau == 1 ? tu : tu + "/" + mau);
+		if(mau < 0) tu = -tu;
+		/* Chuan hoa phan so
+		boolean dau = false;
+		if(tu < 0) {
+			dau = true;
+			tu = -tu;
+		}
+		for(int i = tu; i > 1; i--) {
+			if(mau % i == 0 && tu % i == 0) {
+				mau /= i;
+				tu /= i;
+				break;
+			}
+		}
+		if(dau) tu = -tu;*/
+		System.out.println(tu == 0 ? 0 : mau == 1 ? tu : tu + "/" + mau);
     }
 
     public void nghichDao(){
-	int t	= tu;
-	tu	= mau;
-	mau	= t;
+		int t	= tu;
+		tu	= mau;
+		mau	= t;
     }
 
     public PhanSo giatriNghichDao(){
-	return new PhanSo(mau, tu); 
+		return new PhanSo(mau, tu); 
     }
     
     public double giaTri(){
-	return (double) tu / mau;
+		return (double) tu / mau;
     }
 
     boolean lonHon(PhanSo a){
-	return a.mau * tu > a.tu * mau;
+		return a.mau * tu > a.tu * mau;
     }
 
     PhanSo cong(PhanSo a){
-	return new PhanSo(a.mau * tu + a.tu * mau, mau * a.mau);
+		return new PhanSo(a.mau * tu + a.tu * mau, mau * a.mau);
     }
 
     PhanSo tru(PhanSo a){
-	return new PhanSo(a.mau * tu - a.tu * mau, mau * a.mau);
+		return new PhanSo(a.mau * tu - a.tu * mau, mau * a.mau);
     }
 
     PhanSo nhan(PhanSo a){
-	return new PhanSo(tu * a.tu, mau * a.mau);
+		return new PhanSo(tu * a.tu, mau * a.mau);
     }
 
     PhanSo chia(PhanSo a){
-	return nhan(a.giatriNghichDao());
+		return nhan(a.giatriNghichDao());
     }
 
     PhanSo cong(int a){
-	return new PhanSo(tu + mau * a, mau);
+		return new PhanSo(tu + mau * a, mau);
     }
 
     PhanSo tru(int a){
-	return new PhanSo(tu - mau * a, mau);
+		return new PhanSo(tu - mau * a, mau);
     }
 
     PhanSo nhan(int a){
-	return new PhanSo(tu * a, mau);
+		return new PhanSo(tu * a, mau);
     }
 
     PhanSo chia(int a){
-	return new PhanSo(tu, mau * a);
+		return new PhanSo(tu, mau * a);
     }
 }
