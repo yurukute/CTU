a7b37e9cb52fca661bad83343b815bc01a49756a
yurukute <nguyenkhanhdung.9999@gamil.com>
2023-03-21 19:33:35 +0700

    using dictionary

--- CT287 - KCMH/DFA simulation/dfa_simulation.py
+++ CT287 - KCMH/DFA simulation/dfa_simulation.py
@@ -1,9 +1,9 @@
 class DFA:
     def __init__(self, Q, Sigma, F, delta):
-        self.Q = list(map(int, Q))
+        self.Q = Q
         self.Sigma = Sigma
         self.q0 = self.Q[0]
-        self.F = list(map(int, F))
+        self.F = F
         self.delta = delta
 
     def validate(self, str):
@@ -11,9 +11,7 @@ class DFA:
         for ch in str:
             if ch not in self.Sigma:
                 return 'NO'
-            for transition in self.delta[state]:
-                if ch in transition:
-                    state = self.delta[state].index(transition)
+            state = self.delta[state][ch]
         if state in self.F:
             return 'YES'
         return 'NO'
@@ -21,10 +19,13 @@ class DFA:
 
 n, m = map(int, input().split())
 Q, Sigma, F = input().split(), input().split(), input().split()
-delta = [[[] for i in range(n)] for j in range(n)]
+delta = {}
 for i in range(n*m):
     nstart, char, ndest = input().split()
-    delta[int(nstart)][int(ndest)].append(char)
+    if nstart in delta:
+        delta[nstart].update({char: ndest})
+    else:
+        delta.update({nstart: {char: ndest}})
 
 dfa = DFA(Q, Sigma, F, delta)
 print(dfa.validate(input()))
